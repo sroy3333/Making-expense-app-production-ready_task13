62881260478581e313dabcef422673afb56c2dac
sroy3333 <121767970+sroy3333@users.noreply.github.com>
2024-04-10 00:17:35 +0530

    Update premiumFeature.js

--- controllers/premiumFeature.js
+++ controllers/premiumFeature.js
@@ -1,20 +1,10 @@
 const User = require('../models/users');
-const Expense = require('../models/expenses');
-const sequelize = require('../util/database');
 const e = require('express');
 
 const getUserLeaderBoard = async (req, res) => {
     try {
         const leaderboardofusers = await User.findAll({
-            attributes: ['id', 'name',[sequelize.fn('sum', sequelize.col('expenses.expenseamount')), 'total_cost']],
-            include: [
-                {
-                    model: Expense,
-                    attributes: []
-                }
-            ],
-            group:['user.id'],
-            order: [['total_cost', 'DESC']]
+            order: [['totalExpenses', 'DESC']]
         })
 
         res.status(200).json(leaderboardofusers)
